9251b77a1af15a531768871fe744d28ef8438119
kuririn77680 <gagnon.florian@gmail.com>
2022-08-20 01:55:10 +0200

    test routes.vessel_position

--- tests/routes/test_vessel_position.py
+++ tests/routes/test_vessel_position.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import url_for
 from backend.models.vessel_position import VesselPosition
 
@@ -6,18 +8,31 @@ def test_get_all_vessel_position(app_with_data):
     response = app_with_data.get(url_for("vessel_positions.get_all_vessel_position"))
 
     assert response.status_code == 200
-    vessel_position = VesselPosition.query.all()
-    count = len(vessel_position)
+    vessel_positions = VesselPosition.query.all()
+    count = len(vessel_positions)
+
     assert count == 1
+
+    for vessel_position in vessel_positions:
+        assert vessel_position.vessel_id == 11111
+        assert vessel_position.received_time_utc == datetime(2006, 12, 21, 12, 9, 4)
+        assert vessel_position.latitude == 56.1234
+        assert vessel_position.longitude == -67.3456
 
 
 def test_get_vessel_position(app_with_data):
     response = app_with_data.get(url_for("vessel_positions.get_vessel_position", vessel_id=11111))
 
     assert response.status_code == 200
-    vessel_position = VesselPosition.query.all()
-    count = len(vessel_position)
+    vessel_positions = VesselPosition.query.all()
+    count = len(vessel_positions)
     assert count == 1
+
+    for vessel_position in vessel_positions:
+        assert vessel_position.vessel_id == 11111
+        assert vessel_position.received_time_utc == datetime(2006, 12, 21, 12, 9, 4)
+        assert vessel_position.latitude == 56.1234
+        assert vessel_position.longitude == -67.3456
 
 
 def test_add_vessel_position(app_with_db):
@@ -30,6 +45,13 @@ def test_add_vessel_position(app_with_db):
                                 })
 
     assert response.status_code == 201
-    vessel_position = VesselPosition.query.filter_by(vessel_id=12345).all()
-    count = len(vessel_position)
+    vessel_positions = VesselPosition.query.filter_by(vessel_id=12345).all()
+    count = len(vessel_positions)
     assert count == 1
+
+    for vessel_position in vessel_positions:
+        assert vessel_position.vessel_id == 12345
+        assert vessel_position.received_time_utc == datetime(2016, 12, 21, 12, 9, 4)
+        assert vessel_position.latitude == 70.8765
+        assert vessel_position.longitude == 12.09894
+
